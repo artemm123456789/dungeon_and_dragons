fa1c7f0d526ce3ca7b5a8edd00393027eb2ced72
Dmitriy Poliakov <poliakov.dd@gmail.com>
2026-07-05 19:54:03 +0300

    боевая система

--- systems/battle.py
+++ systems/battle.py
@@ -1,5 +1,6 @@
 import random
-from models.entity import Entity
+from dungeon_and_dragons.models.entity import Entity
+
 
 
 def melee_attack(attacker, defender):
@@ -15,4 +16,33 @@ def melee_attack(attacker, defender):
         При этом должна учитываться защита defender`a
         С некоторым шансом можно сделать критический удар
     """
+    dmg = attacker.attack_power
+    crit_multiplier = 1.5
+    crit_chance = 20
+    actual_damage = dmg
+
+    defender_resist = 0
+    armor_resistance_multiplier = 0.20 #изначальный множитель сопротивления за единицу брони
+    for i in range(defender.defense):
+        missing_absolute_resistance = 1 - defender_resist
+        defender_resist += armor_resistance_multiplier * missing_absolute_resistance
+
+
+    if random.randint(1, 10) <= 6:   #отклонение урона с шансом 60%, урон может варьироваться от 80% attack_power до 120% attack_power
+        deviation_dmg = random.randint(-20, 20)/100
+        actual_damage = round(dmg * (1 + deviation_dmg))
+
+    if random.randint(1, 100) <= crit_chance:   #критические удары с шансом crit_chance
+        actual_damage *= crit_multiplier
+        actual_damage *= (1 - defender_resist)
+        actual_damage = round(actual_damage)
+        defender.take_damage(actual_damage)
+        return f"⚔️ {attacker.name} бьет {defender.name} и наносит критические {actual_damage} урона! (Осталось HP: {defender.hp})"
+
+    actual_damage *= (1 - defender_resist)
+    actual_damage = round(actual_damage)
+    defender.take_damage(actual_damage)
+    return f"⚔️ {attacker.name} бьет {defender.name} на {actual_damage} урона (Осталось HP: {defender.hp})"
+
+
 
